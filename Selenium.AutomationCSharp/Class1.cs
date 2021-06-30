773fc5b6c549ea3953390ebd2238d6d59762abeb
ravit <ravit@LAPTOP-UJDTFV4P>
2021-06-30 11:12:33 +0530

    Add project files.

---
+++ Selenium.AutomationCSharp/Class1.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace Selenium.AutomationCSharp
+{
+    public class Class1
+    {
+
+        [Test]
+        public void invokeBrowser()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            ChromeDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://facebook.com");
+            driver.Manage().Window.Maximize();
+
+
+        }
+    }
+}
